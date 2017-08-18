1934581ed4e98cef4b3e47587547baad0acdd2e3
Sagold <daphneepatenaude@gmail.com>
2017-08-18 10:10:55 -0400

    add the ZXing librairy, create the controller and add event in the mainActivity

---
+++ app/src/main/java/com/sag0ld/barcodegenerator/Controller.kt
@@ -0,0 +1,18 @@
+package com.sag0ld.barcodegenerator
+
+import android.graphics.Bitmap
+
+/**
+ * Created by Sagold on 2017-08-17.
+ */
+class Controller private constructor(){
+    private object Holder { val INSTANCE = Controller()}
+
+    companion object {
+        val instance : Controller by lazy {Holder.INSTANCE}
+    }
+
+    fun generateBarcode (type : String, content : String) {
+
+    }
+}
--- app/src/main/java/com/sag0ld/barcodegenerator/MainActivity.kt
+++ app/src/main/java/com/sag0ld/barcodegenerator/MainActivity.kt
@@ -1,12 +1,57 @@
 package com.sag0ld.barcodegenerator
 
+import android.graphics.Bitmap
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.ArrayAdapter
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Spinner
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Set the barcode as imageView
+        val barcodeView : ImageView = findViewById(R.id.barcodeView)
+
+        fun setBarcodeImage (b : Bitmap) {
+            barcodeView.setImageBitmap(b)
+        }
+
+        // Init the barcode spinner
+        val barcodeTypeSpinner : Spinner = findViewById(R.id.barcodeTypeSpinner)
+        val adapter : ArrayAdapter<CharSequence> = ArrayAdapter.createFromResource(this,
+                            R.array.type_of_barcode,android.R.layout.simple_spinner_item)
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        barcodeTypeSpinner.adapter = adapter
+
+        barcodeTypeSpinner.setOnItemClickListener { adapterView, view, i, l ->
+            setBarcodeImage(Controller.instance.generateBarcode(barcodeTypeSpinner.selectedItem.toString() ,
+                    contentEditText.text.toString()))
+        }
+        // Init the content editText
+        val contentEditText : EditText = findViewById(R.id.contentEditText)
+
+        contentEditText.addTextChangedListener( object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                setBarcodeImage(Controller.instance.generateBarcode(barcodeTypeSpinner.selectedItem.toString() ,
+                        p0.toString()))
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+        })
     }
 }
