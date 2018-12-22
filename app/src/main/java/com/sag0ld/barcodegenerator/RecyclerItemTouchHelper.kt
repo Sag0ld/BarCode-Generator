0dd9393942fc129365432417490f60659297bf2e
Sag0ld <daphneepatenaude@gmail.com>
2018-12-22 03:56:32 -0500

    add new launcher icon. add dialog on long press to delete a barcode from the history. add a swip right to delete item.

---
+++ app/src/main/java/com/sag0ld/barcodegenerator/RecyclerItemTouchHelper.kt
@@ -0,0 +1,54 @@
+package com.sag0ld.barcodegenerator
+
+import android.graphics.Canvas
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.helper.ItemTouchHelper
+
+class RecyclerItemTouchHelper(dragDirs: Int, swipeDirs: Int, val listener: RecyclerItemTouchHelperListener)
+    : ItemTouchHelper.SimpleCallback(dragDirs, swipeDirs) {
+
+    override fun onMove(recyclerView: RecyclerView?, viewHolder: RecyclerView.ViewHolder?,
+                        target: RecyclerView.ViewHolder?): Boolean {
+        return true
+    }
+
+    override fun onSwiped(viewHolder: RecyclerView.ViewHolder?, direction: Int) {
+        viewHolder?.let {
+            listener.onSwiped(it, direction, viewHolder.getAdapterPosition())
+        }
+    }
+
+    override fun onSelectedChanged(viewHolder: RecyclerView.ViewHolder?, actionState: Int) {
+        if (viewHolder != null) {
+            val foregroundView = (viewHolder as BarcodeAdapter.BarcodeViewHolder).viewForeground
+
+            ItemTouchHelper.Callback.getDefaultUIUtil().onSelected(foregroundView)
+        }
+    }
+
+    override fun onChildDrawOver(c: Canvas, recyclerView: RecyclerView,
+                                 viewHolder: RecyclerView.ViewHolder, dX: Float, dY: Float,
+                                 actionState: Int, isCurrentlyActive: Boolean) {
+        val foregroundView = (viewHolder as BarcodeAdapter.BarcodeViewHolder).viewForeground
+        ItemTouchHelper.Callback.getDefaultUIUtil().onDrawOver(c, recyclerView, foregroundView, dX, dY,
+                actionState, isCurrentlyActive)
+    }
+
+    override fun clearView(recyclerView: RecyclerView?, viewHolder: RecyclerView.ViewHolder) {
+        val foregroundView = (viewHolder as BarcodeAdapter.BarcodeViewHolder).viewForeground
+        ItemTouchHelper.Callback.getDefaultUIUtil().clearView(foregroundView)
+    }
+
+    override fun onChildDraw(c: Canvas, recyclerView: RecyclerView,
+                    viewHolder: RecyclerView.ViewHolder, dX: Float, dY: Float,
+                    actionState: Int, isCurrentlyActive: Boolean) {
+        val foregroundView = (viewHolder as BarcodeAdapter.BarcodeViewHolder).viewForeground
+
+        ItemTouchHelper.Callback.getDefaultUIUtil().onDraw(c, recyclerView, foregroundView, dX, dY,
+                actionState, isCurrentlyActive)
+    }
+
+    interface RecyclerItemTouchHelperListener {
+        fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int, position: Int)
+    }
+}
